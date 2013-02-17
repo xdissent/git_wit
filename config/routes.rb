072912da70ccacb28141233de616d26ecc712763
Greg Thornton <xdissent@me.com>
2013-02-17 17:59:47 -0600

    Be paranoid in regex

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 GitWit::Engine.routes.draw do
-  get ":repository/*refs" => "git#service", repository: /[-\/\w\.]+\.git/
-  post ":repository/:service" => "git#service", repository: /[-\/\w\.]+\.git/, 
+  get ":repository/*refs" => "git#service", repository: /[\-\/\w\.]+\.git/
+  post ":repository/:service" => "git#service", repository: /[\-\/\w\.]+\.git/, 
     service: /git-[\w\-]+/
 end
