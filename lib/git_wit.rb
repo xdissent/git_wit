97bd4dbafd1cf536f847a80093a2be9f68c33e86
Greg Thornton <xdissent@me.com>
2013-02-17 22:57:34 -0600

    Move actual config defaults into module.

--- lib/git_wit.rb
+++ lib/git_wit.rb
@@ -10,4 +10,29 @@ module GitWit
 
   config_accessor :repositories_path, :ssh_user, :realm,
     :git_http_backend_path, :insecure_write, :insecure_auth
+
+  def self.reset_config!
+    @_config = nil
+  end
+
+  def self.stash_config
+    @_stashed = @_config.dup
+  end
+
+  def self.restore_config
+    @_config = @_stashed
+    @_stashed = nil
+  end
+
+  def self.default_config!
+    reset_config!
+    configure do |config|
+      config.realm = "GitWit"
+      config.repositories_path = "/var/git"
+      config.ssh_user = "git"
+      config.git_http_backend_path = "/usr/libexec/git-core/git-http-backend"
+      config.insecure_write = false
+      config.insecure_auth = false
+    end
+  end
 end
