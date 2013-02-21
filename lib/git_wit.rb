e4f23960cd2e9e5ed07e4931eb38bae321696978
Greg Thornton <gthornton@cjadvertising.com>
2013-02-21 16:25:19 -0600

    Make config object only available in config block.

--- lib/git_wit.rb
+++ lib/git_wit.rb
@@ -1,15 +1,18 @@
+require "authorized_keys"
 require "active_support/configurable"
 require "git_wit/engine"
 require "git_wit/errors"
 require "git_wit/auth"
 require "git_wit/shell"
 require "git_wit/authorized_keys"
+require "git_wit/authorized_keys/key"
+require "git_wit/authorized_keys/file"
 
 module GitWit
   include ActiveSupport::Configurable
 
   config_accessor :repositories_path, :ssh_user, :realm,
-    :git_http_backend_path, :insecure_write, :insecure_auth
+    :git_path, :insecure_write, :insecure_auth
 
   def self.reset_config!
     @_config = nil
@@ -28,11 +31,15 @@ module GitWit
     reset_config!
     configure do |config|
       config.realm = "GitWit"
-      config.repositories_path = "/var/git"
-      config.ssh_user = "git"
-      config.git_http_backend_path = "/usr/libexec/git-core/git-http-backend"
+      config.repositories_path = Rails.root.join("repositories").to_s
+      config.ssh_user = nil
+      config.git_path = "git"
       config.insecure_write = false
       config.insecure_auth = false
     end
+  end
+
+  class << self
+    private :config
   end
 end
